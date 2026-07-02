684500ff851df834ea64caad56b0d736071c69e4
Nikita Mazurenko <mazurenko.nikita.mn@gmail.com>
2026-07-02 21:32:21 +0200

    pytest passed

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ class Distance:
 
     def __mul__(self, other : int | float) -> Distance:
         if isinstance(other, (int, float)):
-            return Distance(km=round(self.km * other, 2))
+            return Distance(km=self.km * other)
         return NotImplemented
 
     def __truediv__(self, other : int | float) -> Distance:
@@ -38,35 +38,35 @@ class Distance:
             return Distance(km=round((self.km / other), 2))
         return NotImplemented
 
-    def __lt__(self, other : Distance | int) -> bool:
+    def __lt__(self, other : Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         if isinstance(other, (int, float)):
             return self.km < other
         return NotImplemented
 
-    def __gt__(self, other : Distance | int) -> bool:
+    def __gt__(self, other : Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         if isinstance(other, (int, float)):
             return self.km > other
         return NotImplemented
 
-    def __eq__(self, other: Distance | int) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         if isinstance(other, (int, float)):
             return self.km == other
         return NotImplemented
 
-    def __le__(self, other: Distance | int) -> bool:
+    def __le__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         if isinstance(other, (int, float)):
             return self.km <= other
         return NotImplemented
 
-    def __ge__(self, other: Distance | int) -> bool:
+    def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         if isinstance(other, (int, float)):
